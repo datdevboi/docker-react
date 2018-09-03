111317c3c226be8643d81f5d2348b05d028cc8e0
camilo17 <cami172013@gmail.com>
2018-09-03 10:10:46 -0500

    chagned app text

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ class App extends Component {
           <h1 className="App-title">Welcome to Dockers</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          I was changed into
         </p>
       </div>
     );
